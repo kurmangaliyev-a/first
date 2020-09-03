dcd7fd23ac44eac9f535bd59ada2a34ebc829dc1
KurmangaliyevAzamat <kurmangaliev.azamat1996@gmail.com>
2020-09-03 10:00:10 +0600

    error in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello/title>
+    <title>Hello</title>
 </head>
 <body>
 <p>
